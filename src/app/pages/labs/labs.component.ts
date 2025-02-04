38a0945f458b07d42d6ca97de7057d7ff2764b54
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2025-02-04 14:44:01 -0400

    añadiendo changeName y usando un switch

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -34,6 +34,16 @@ export class LabsComponent {
     const input=event.target as HTMLInputElement;
     console.log(input.value);
   }
+  changeName(event: Event){
+    const input= event.target as HTMLInputElement;
+    const newValue= input.value;
+    this.person.update(prevState=>{
+      return{
+        ...prevState,
+        name: newValue
+      }
+    });
+  }
   changeAge(event: Event){
     const input= event.target as HTMLInputElement;
     const newValue= input.value;
